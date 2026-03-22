1ad24f4e7d15b464616e1810b889bd2b263e0b21
Jordan Juel <jordan.juel@gmail.com>
2026-03-22 17:57:59 -0500

    Implementing handler for getting a team by an id.
    This implements the GetTeamByIDHandler function. This will allow us to get
    a team from the database by a known id.

--- internal/store/store.go
+++ internal/store/store.go
@@ -16,7 +16,10 @@ func NewStore(db *sql.DB) *Store {
 
 func (s *Store) GetAllTeams() ([]models.Team, error) {
 	log.Printf("Fetching all teams from database")
-	query := "SELECT id, cfbd_id, school, mascot, abbreviation, conference, division, classification, city, state FROM teams"
+	query := `
+		SELECT id, cfbd_id, school, mascot, abbreviation, conference, division, classification, city, state 
+		FROM teams
+	`
 	rows, err := s.db.Query(query)
 	if err != nil {
 		log.Printf("Error querying teams: %v", err)
@@ -27,7 +30,17 @@ func (s *Store) GetAllTeams() ([]models.Team, error) {
 	var teams []models.Team
 	for rows.Next() {
 		var t models.Team
-		err := rows.Scan(&t.ID, &t.CfbdID, &t.School, &t.Mascot, &t.Abbreviation, &t.Conference, &t.Division, &t.Classification, &t.City, &t.State)
+		err := rows.Scan(
+			&t.ID,
+			&t.CfbdID,
+			&t.School,
+			&t.Mascot,
+			&t.Abbreviation,
+			&t.Conference,
+			&t.Division,
+			&t.Classification,
+			&t.City,
+			&t.State)
 		if err != nil {
 			log.Printf("Error scanning team row: %v", err)
 			return nil, err
@@ -42,4 +55,38 @@ func (s *Store) GetAllTeams() ([]models.Team, error) {
 
 	log.Printf("Successfully fetched %d teams from database", len(teams))
 	return teams, nil
+}
+
+func (s *Store) GetTeamById(id int) (models.Team, error) {
+	log.Printf("Fetching team by id: %d", id)
+	query := `
+		SELECT id, cfbd_id, school, mascot, abbreviation, conference, division, classification, city, state 
+		FROM teams 
+		WHERE id = ?
+	`
+	var team models.Team
+	err := s.db.QueryRow(query, id).Scan(
+		&team.ID,
+		&team.CfbdID,
+		&team.School,
+		&team.Mascot,
+		&team.Abbreviation,
+		&team.Conference,
+		&team.Division,
+		&team.Classification,
+		&team.City,
+		&team.State,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			log.Printf("No team with id %d found in db", id)
+			return models.Team{}, err
+		}
+		log.Printf("Error fetching team from db %v", err)
+		return models.Team{}, err
+	}
+
+	log.Printf("Successfully fetched team with id: %d from db", id)
+	return team, nil
 }
